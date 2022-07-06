7af22a7d7f9bcd85f4b505077a048413f339b37a
gifariramadhan <gifariramadhan.gr@gmail.com>
2022-07-07 03:08:15 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# robofriends PWA
+
+adding PWA capabilities to a react app.
+
+1. Clone this repo
+2. Run `npm install`
+3. Run `npm start`
--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { searchRobots, requestRobots } from './reducers';
+import 'tachyons';
+
+const logger = createLogger()
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
